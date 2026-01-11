87c17d1455501929ad7c46a4c84aadceeee23986
Jeff Njoroge <60646637+jeff283@users.noreply.github.com>
2026-01-11 15:07:24 +0300

    Implement main function in index.ts to handle action inputs and execute the main logic, replacing the initial console log.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import * as core from "@actions/core";
+import { main, type InfisicalActionInputs } from "@/action";
+
+async function run() {
+  try {
+    // Inputs
+    // const infisicalClientId = core.getInput("infisical-client-id");
+    // const infisicalClientSecret = core.getInput("infisical-client-secret");
+    // const infisicalProjectSlug = core.getInput("infisical-project-slug");
+    // const infisicalDomain = core.getInput("infisical-domain");
+    // const infisicalEnvSlug = core.getInput("infisical-env-slug");
+
+    const inputs: InfisicalActionInputs = {
+      infisicalClientId: process.env.INFISICAL_CLIENT_ID || "",
+      infisicalClientSecret: process.env.INFISICAL_CLIENT_SECRET || "",
+      infisicalProjectSlug: process.env.INFISICAL_PROJECT_SLUG || "",
+      infisicalProjectId: process.env.INFISICAL_PROJECT_ID || "",
+      infisicalDomain: process.env.INFISICAL_DOMAIN || "",
+      infisicalEnvSlug: process.env.INFISICAL_ENV_SLUG || "",
+    };
+    await main(inputs);
+  } catch (error) {
+    const e = error as Error;
+    core.setFailed(e.message);
+  }
+}
+
+run();
